61777e445ee6d1b9a56a4d2cc70f616cddcf7b3c
dbilgin <denizcanbilgin@gmail.com>
2021-11-30 04:15:07 +0100

    fixed platform check for web

--- lib/util/update_widget.dart
+++ lib/util/update_widget.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:home_widget/home_widget.dart';
 
 Future<void> updateWidget(String notes) async {
-  if (!Platform.isAndroid) {
+  if (kIsWeb || !Platform.isAndroid) {
     return;
   }
   await HomeWidget.saveWidgetData<String>('_notes', notes);
