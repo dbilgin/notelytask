f823a49abd54d7fb2f10459163764158669f86da
dbilgin <denizcanbilgin@gmail.com>
2024-03-24 15:47:05 +0100

    fixed loader & separated theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:notelytask/screens/home_page.dart';
 import 'package:notelytask/cubit/notes_cubit.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:notelytask/service/navigation_service.dart';
+import 'package:notelytask/utils.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -60,75 +61,7 @@ class App extends StatelessWidget {
           ),
           child: MaterialApp(
             title: 'NotelyTask',
-            theme: ThemeData(
-              elevatedButtonTheme: ElevatedButtonThemeData(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                    const Color(0xff17181c),
-                  ),
-                  foregroundColor: MaterialStateProperty.all(
-                    const Color(0xffdce3e8),
-                  ),
-                ),
-              ),
-              textButtonTheme: TextButtonThemeData(
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all(
-                    const Color(0xffdce3e8),
-                  ),
-                ),
-              ),
-              snackBarTheme: const SnackBarThemeData(
-                contentTextStyle: TextStyle(
-                  color: Color(0xffdce3e8),
-                ),
-                backgroundColor: Color(0xff17181c),
-              ),
-              floatingActionButtonTheme: const FloatingActionButtonThemeData(
-                foregroundColor: Color(0xffdce3e8),
-              ),
-              brightness: Brightness.dark,
-              primarySwatch: Colors.blue,
-              colorScheme: const ColorScheme.dark(
-                primary: Color(0xff17181c),
-                secondary: Color(0xff2e8fff),
-              ),
-              textSelectionTheme: const TextSelectionThemeData(
-                cursorColor: Color(0xffdce3e8),
-                selectionColor: Color(0xff2e8fff),
-                selectionHandleColor: Color(0xff2e8fff),
-              ),
-              hintColor: const Color(0xffdce3e8),
-              inputDecorationTheme: const InputDecorationTheme(
-                labelStyle: TextStyle(color: Color(0xffdce3e8)),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xffdce3e8)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xffdce3e8)),
-                ),
-              ),
-              scaffoldBackgroundColor: const Color(0xff1f1f24),
-              textTheme: const TextTheme(
-                titleLarge: TextStyle(
-                  color: Color(0xffdce3e8),
-                  fontWeight: FontWeight.bold,
-                ),
-                headlineMedium: TextStyle(
-                  color: Color(0xffdce3e8),
-                  fontWeight: FontWeight.bold,
-                ),
-                titleMedium: TextStyle(
-                  color: Color(0xffdce3e8),
-                  fontWeight: FontWeight.bold,
-                ),
-                bodyLarge: TextStyle(
-                  color: Color(0xffdce3e8),
-                  fontSize: 16.0,
-                ),
-                bodySmall: TextStyle(color: Color(0xffdce3e8)),
-              ),
-            ),
+            theme: themeData,
             navigatorKey: getIt<NavigationService>().navigatorKey,
             initialRoute: '/',
             onGenerateRoute: (RouteSettings settings) {
